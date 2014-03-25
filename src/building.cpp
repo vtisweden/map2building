965a7a8151217b6135c300714713b26b1d2afa99
Björn Blissing <bjorn.blissing@vti.se>
2014-03-25 23:00:03 +0100

    Basic building of walls.

--- src/building.cpp
+++ src/building.cpp
@@ -1,6 +1,9 @@
 #include "building.h"
 
 #include <osg/Notify>
+#include <osg/Geometry>
+#include <osgUtil/Tessellator>
+#include <osgUtil/SmoothingVisitor>
 
 void Building::load(pugi::xml_node buildingNode) {
 	
@@ -37,3 +40,63 @@ void Building::load(pugi::xml_node buildingNode) {
 	osg::notify(osg::DEBUG_INFO) << "Material sets:     " << m_materials.size() << std::endl;
 	osg::notify(osg::DEBUG_INFO) << std::endl;
 }
+
+osg::ref_ptr<osg::Group> Building::createFromPolygon(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate) {
+	osg::ref_ptr<osg::Group> building = new osg::Group;
+	// Build basement
+	building->addChild(buildBasement(polygon, baseCoordinate));
+	// Build walls
+
+	// Build windows
+	
+	
+	return building;
+}
+
+osg::ref_ptr<osg::Geode> Building::buildBasement(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate) {
+	osg::ref_ptr<osg::Geode> basementGeode = new osg::Geode;
+	osg::ref_ptr<osg::Geometry> basementGeometry = buildWall(polygon, baseCoordinate, m_basementHeight);
+	basementGeode->addDrawable(basementGeometry);
+	return basementGeode;
+}
+
+osg::ref_ptr<osg::Geometry> Building::buildWall(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, double height) {
+	// Collect the verticies
+	osg::ref_ptr<osg::Vec3Array> vertexArray = new osg::Vec3Array;
+	osg::Vec2Array::iterator it;
+	// 
+	for (it = polygon->points()->begin(); it != polygon->points()->end(); ++it) {
+		osg::Vec2 p = (*it);
+		vertexArray->push_back(osg::Vec3(p.x() - baseCoordinate.x(), polygon->height(), p.y()-baseCoordinate.y()));
+	}
+	size_t numberVerticiesInLayer = vertexArray->size();
+	// Extrude
+	for (it = polygon->points()->begin(); it != polygon->points()->end(); ++it) {
+		osg::Vec2 p = (*it);
+		vertexArray->push_back(osg::Vec3(p.x() - baseCoordinate.x(), polygon->height() + height, p.y()-baseCoordinate.y()));
+	}
+	osg::ref_ptr<osg::Geometry> wallGeometry = new osg::Geometry;
+	wallGeometry->setVertexArray(vertexArray);
+	wallGeometry->addPrimitiveSet(new osg::DrawArrays(GL_POLYGON, 0 ,numberVerticiesInLayer));
+	wallGeometry->addPrimitiveSet(new osg::DrawArrays(GL_POLYGON, numberVerticiesInLayer ,numberVerticiesInLayer));
+
+	osgUtil::Tessellator tesselator;
+	tesselator.setTessellationType(osgUtil::Tessellator::TESS_TYPE_POLYGONS);
+	tesselator.setWindingType(osgUtil::Tessellator::TESS_WINDING_ODD);
+	tesselator.retessellatePolygons(*wallGeometry);
+
+	osg::ref_ptr<osg::DrawElementsUInt> sideIndices = new osg::DrawElementsUInt(GL_QUAD_STRIP);
+	for (size_t i = 0; i < numberVerticiesInLayer; ++i) {
+		sideIndices->push_back(i);
+		sideIndices->push_back((numberVerticiesInLayer-1-i) + numberVerticiesInLayer);
+	}
+	sideIndices->push_back(0);
+	sideIndices->push_back(numberVerticiesInLayer*2 - 1);
+
+	wallGeometry->addPrimitiveSet(sideIndices);
+
+	osgUtil::SmoothingVisitor::smooth(*wallGeometry);
+
+	return wallGeometry;
+}
+
--- src/building.h
+++ src/building.h
@@ -4,7 +4,10 @@
 #include <vector>
 
 #include <osg/Referenced>
+#include <osg/Group>
+#include <osg/Geode>
 
+#include "polygon.h"
 #include "pugixml.hpp"
 
 struct MaterialSet {
@@ -18,11 +21,19 @@ class Building : public osg::Referenced {
 public:
 	Building() : m_roofHeight(0.0), m_basementHeight(0.0), m_windowOffset(0.0) {};
 	void load(pugi::xml_node buildingNode);
+	osg::ref_ptr<osg::Group> createFromPolygon(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate);
 protected:
 	~Building() {};
+	osg::ref_ptr<osg::Geode> buildBasement(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate);
+	osg::ref_ptr<osg::Geometry> buildWall(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, double height);
+
 	double m_roofHeight;
 	double m_basementHeight;
 	double m_windowOffset;
 	std::vector<MaterialSet> m_materials;
 };
+
+typedef std::map< double, osg::ref_ptr<Building> > BuildingMap;
+typedef BuildingMap::iterator BuildingMapIterator;
+
 #endif
