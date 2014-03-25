1d36e8c83ebb421d60594261f97e319883f40b49
Björn Blissing <bjorn.blissing@vti.se>
2014-03-25 17:01:01 +0100

    Fixed loading of polygons in shapeworld

--- src/shapeworld.cpp
+++ src/shapeworld.cpp
@@ -24,165 +24,76 @@ bool ShapeWorld::loadShapeFile(const std::string& filename) {
 	int	numberOfBuildings = ogrLayer->GetFeatureCount();
 	osg::notify(osg::ALWAYS) << "Number of buildings in shape file: " << numberOfBuildings << std::endl;
 	
-	//Find boundaries of layer features
-	findWorldBounds(ogrLayer);
+	// Load polygons
+	loadPolygons(ogrLayer);
 
-	m_tilesize = 5000.0;
-	int tiles = 0;
-	osg::ref_ptr<osg::Group> root = new osg::Group;
-	for (double px = m_minX; px < m_maxX; px += m_tilesize) {
-		for (double py = m_minY; py < m_maxY; py += m_tilesize) {
-			tiles++;
-			root->addChild(buildTile(ogrLayer, px, py));
-		}
-	}
-	osg::notify(osg::ALWAYS) << "Number of tiles: " << tiles << std::endl;
+	//Find boundaries of layer features
+	findWorldBounds();
 
 	return true;
 }
 
-void ShapeWorld::findWorldBounds(OGRLayer* layer)
+void ShapeWorld::findWorldBounds()
 {
-	OGRFeature* poFeature;
-	while( ( poFeature = layer->GetNextFeature()) != NULL ) {
-		OGRGeometry* poGeometry = poFeature->GetGeometryRef();
+	PolygonVectorIterator it;
+	for (it = m_polygons.begin(); it != m_polygons.end(); ++it) {
+		osg::ref_ptr<Polygon> polygon = (*it);
+		double centerX = polygon->center().x();
+		double centerY = polygon->center().y();
 
-		if( poGeometry != NULL && wkbFlatten(poGeometry->getGeometryType()) == wkbPolygon )	{
-			OGRPolygon* poPolygon = static_cast<OGRPolygon *>(poGeometry);					
-			OGRLinearRing* poLinearRing = poPolygon->getExteriorRing();
+		// Find max and min of X-coordinate
+		if ( centerX > m_maxX )	{
+			m_maxX = centerX;
+		} else if ( centerX < m_minX ) {
+			m_minX = centerX;
+		}
 
-			double centerX = 0;
-			double centerY = 0;
-
-			findPolygonCenter(poLinearRing, centerX, centerY);
-			
-
-			// Find max and min of X-coordinate
-			if ( centerX > m_maxX )	{
-				m_maxX = centerX;
-			} else if ( centerX < m_minX ) {
-				m_minX = centerX;
-			}
-
-			// Find max and min of Y-coordinate
-			if ( centerY > m_maxY )	{
-				m_maxY = centerY;
-			} else if ( centerY < m_minY ) {
-				m_minY = centerY;
-			}
+		// Find max and min of Y-coordinate
+		if ( centerY > m_maxY )	{
+			m_maxY = centerY;
+		} else if ( centerY < m_minY ) {
+			m_minY = centerY;
 		}
 	}
-	osg::notify(osg::ALWAYS) << "Found bound: X: " << m_minX <<"\t" << m_maxX << std::endl;
-	osg::notify(osg::ALWAYS) << "             Y: " << m_minY <<"\t" << m_maxY << std::endl;
+
+	osg::notify(osg::DEBUG_INFO) << "Found bound: X: " << m_minX <<"\t" << m_maxX << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "             Y: " << m_minY <<"\t" << m_maxY << std::endl;
 }
 
-void ShapeWorld::findPolygonCenter(OGRLinearRing* linearRing, double& centerX, double& centerY) {
-	// Get initial coordinates
-	OGRPoint* poPoint = static_cast<OGRPoint*>(OGRGeometryFactory::createGeometry( wkbPoint ));
-	linearRing->getPoint(0,poPoint);
-	double minX = poPoint->getX();
-	double maxX = minX;
-
-	double minY = poPoint->getY();
-	double maxY = minY;
-
-	// Get bounding points of building
-	for( int i=0; i < linearRing->getNumPoints(); ++i ) {
-		linearRing->getPoint(i,poPoint);	
-
-		double pointX = poPoint->getX();
-		double pointY = poPoint->getY();
-
-		if ( pointX > maxX )
-		{
-			maxX = pointX;
-		}
-		else if ( pointX < minX )
-		{
-			minX = pointX;
-		}
-
-		if ( pointY > maxY )
-		{
-			maxY = pointY;
-		}
-		else if ( pointY < minY )
-		{
-			minY = pointY;
-		}
-	}
-	
-	OGRGeometryFactory::destroyGeometry(poPoint);
-
-	// Calculate center point of building
-	centerX = ( minX + maxX ) / 2.0;
-	centerY = ( minY + maxY ) / 2.0;
-}
-
-osg::ref_ptr<osg::Group> ShapeWorld::buildTile(OGRLayer* layer, double px, double py) {
-
-	// Calculate center point of tile
-	//double centerX = px + m_tilesize / 2.0;
-	//double centerY = px + m_tilesize / 2.0;
-
-	osg::ref_ptr<osg::Group> tileGroup = new osg::Group;	
-	std::stringstream tilename;
-	tilename.clear();
-	tilename << "Tile: " << px << ":" << py;
-	tileGroup->setName(tilename.str());
-
+void ShapeWorld::loadPolygons(OGRLayer* layer) {
 	//Reset before classifying
 	layer->ResetReading();
 
 	int numberOfBuildings = layer->GetFeatureCount();
-	int buildingInTile = 0;
 	for (int h = 0; h < numberOfBuildings; ++h) {
-		OGRFeature* poFeature;
-		poFeature = layer->GetFeature( h );
+		OGRFeature* ogrFeature;
+		ogrFeature = layer->GetFeature( h );
 
-		OGRGeometry *poGeometry;			
-		poGeometry = poFeature->GetGeometryRef();
+		OGRGeometry* ogrGeometry;			
+		ogrGeometry = ogrFeature->GetGeometryRef();
 
-		OGRPolygon *poPolygon = (OGRPolygon *) poGeometry;					
-		OGRLinearRing *poLinearRing = poPolygon->getExteriorRing();
+		OGRPolygon* ogrPolygon = (OGRPolygon *) ogrGeometry;					
+		OGRLinearRing* ogrLinearRing = ogrPolygon->getExteriorRing();
 
-		double cx = 0.0;
-		double cy = 0.0;
-		findPolygonCenter(poLinearRing, cx, cy);
-
-		//Check if found building is within the tile
-		if ((cx >= px) && (cx < px + m_tilesize) && (cy >= py) && (cy < py + m_tilesize) ) {
-			osg::ref_ptr<osg::Group> building = addBuilding(poLinearRing, cx, cy);
-			tileGroup->addChild(building);
-			buildingInTile++;
-			
-		} else {
-			//osg::notify(osg::ALWAYS) << "\tOutside" << std::endl;
-		}
+		osg::ref_ptr<Polygon> polygon = buildPolygon(ogrLinearRing);
 		
+		m_polygons.push_back(polygon);
 	}
-	osg::notify(osg::ALWAYS) << "Building in tile: " << buildingInTile << std::endl;
-
-	return tileGroup;
 }
 
-osg::ref_ptr<osg::Group> ShapeWorld::addBuilding(OGRLinearRing* linearRing, double cx, double cy) {
+osg::ref_ptr<Polygon> ShapeWorld::buildPolygon(OGRLinearRing* linearRing) {
+	osg::ref_ptr<Polygon> polygon = new Polygon;
 	//Add buildings to node
 	OGRPoint* point = static_cast<OGRPoint*>(OGRGeometryFactory::createGeometry( wkbPoint ));
-
-	osg::ref_ptr<osg::Vec3Array> floorCoord = new osg::Vec3Array;
 
 	for(int i=0; i<linearRing->getNumPoints(); ++i)
 	{
 		linearRing->getPoint(i, point);	
-
-		float newX = point->getX() - cx;
-		float newY = point->getY() - cy;
-		floorCoord->push_back(osg::Vec3( newX, 0.0, newY )); 
+		polygon->push_back(osg::Vec2( point->getX(), point->getY())); 
 	}
-	
+
 	OGRGeometryFactory::destroyGeometry(point);
 
-	//return BuildingLibrary::createBuilding(osg::Vec3Array floorCoord);
-	return 0;
+	return polygon;
 }
+
--- src/shapeworld.h
+++ src/shapeworld.h
@@ -3,28 +3,26 @@
 
 #include <string>
 
-#include <osg/Referenced>
-#include <osg/Group>
-
 #include <gdal/ogrsf_frmts.h>
 
+#include "polygon.h"
 
 class ShapeWorld : public osg::Referenced {
 public:
 	ShapeWorld() : m_minX(std::numeric_limits<double>::max()), m_maxX(std::numeric_limits<double>::min()), m_minY(std::numeric_limits<double>::max()), m_maxY(std::numeric_limits<double>::min()) {};
 	bool loadShapeFile(const std::string& filename);
+	PolygonVector polygons() const { return m_polygons; }
 protected:
 	~ShapeWorld() {};
-	void findWorldBounds(OGRLayer* inLayer);
-	void findPolygonCenter(OGRLinearRing* linearRing, double& x, double& y);
-	osg::ref_ptr<osg::Group> addBuilding(OGRLinearRing* linearRing, double cx, double cy);
-	osg::ref_ptr<osg::Group> buildTile(OGRLayer* layer, double px, double py);
-	
+	void findWorldBounds();
+	void loadPolygons(OGRLayer* layer);
+	osg::ref_ptr<Polygon> buildPolygon(OGRLinearRing* linearRing);
+
 	double m_minX;
 	double m_maxX;
 	double m_minY;
 	double m_maxY;
 
-	double m_tilesize;
+	PolygonVector m_polygons;
 };
 #endif
