ebe5d4b7e9f5a67ca871773f416b19a7de3dc658
Björn Blissing <bjorn.blissing@vti.se>
2014-03-31 17:23:34 +0200

    Added windows to buildings.

--- src/building.cpp
+++ src/building.cpp
@@ -9,24 +9,41 @@
 #include <osg/Notify>
 #include <osg/Geometry>
 #include <osgUtil/Tessellator>
-#include <osgUtil/SmoothingVisitor>
 
 #include "materiallibrary.h"
 
+osg::ref_ptr<osg::PolygonOffset> Building::m_polygonOffset = 0;
+
+Building::Building() : m_basementHeight(1.0), 
+	m_floorHeight(3.0), m_numberOfFloors(2), 
+	m_windowHorizontalSpacing(1.0), m_windowVerticalSpacing(1.0),
+	m_windowHeight(2.0), m_windowWidth(1.0), m_type("Undefined")
+{
+	if (!m_polygonOffset.valid()) {
+		m_polygonOffset = new osg::PolygonOffset;
+		m_polygonOffset->setFactor(-1.0f);
+		m_polygonOffset->setUnits(-1.0f);
+	}
+}
+
 void Building::load(pugi::xml_node buildingNode)
 {
-	std::string type = buildingNode.attribute("type").as_string("");
+	m_type = buildingNode.attribute("type").as_string("Undefined");
 
 	if (pugi::xml_node basementNode = buildingNode.child("basement")) {
-		m_basementHeight = basementNode.attribute("height").as_double(0.0);
+		m_basementHeight = basementNode.attribute("height").as_double(2.0);
 	}
 
-	if (pugi::xml_node roofNode = buildingNode.child("roof")) {
-		m_roofHeight = roofNode.attribute("height").as_double(0.0);
+	if (pugi::xml_node floorNode = buildingNode.child("floor")) {
+		m_floorHeight = floorNode.attribute("height").as_double(3.0);
+		m_numberOfFloors = floorNode.attribute("number").as_uint(1);
 	}
 
 	if (pugi::xml_node windowNode = buildingNode.child("window")) {
-		m_windowOffset = windowNode.attribute("offset").as_double(0.0);
+		m_windowWidth = windowNode.attribute("width").as_double(1.0);
+		m_windowHeight = windowNode.attribute("height").as_double(2.0);
+		m_windowHorizontalSpacing = windowNode.attribute("hspace").as_double(1.0);
+		m_windowVerticalSpacing = windowNode.attribute("vspace").as_double(1.0);
 	}
 
 	if (pugi::xml_node materialsNode = buildingNode.child("materials")) {
@@ -41,10 +58,13 @@ void Building::load(pugi::xml_node buildingNode)
 		}
 	}
 
-	osg::notify(osg::DEBUG_INFO) << "Building Type:     " << type << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "Building Type:     " << m_type << std::endl;
 	osg::notify(osg::DEBUG_INFO) << "Basement height:   " << m_basementHeight << std::endl;
-	osg::notify(osg::DEBUG_INFO) << "Roof height:       " << m_roofHeight << std::endl;
-	osg::notify(osg::DEBUG_INFO) << "Window offset:     " << m_windowOffset << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "Floor height:      " << m_floorHeight << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "Number of floors:  " << m_numberOfFloors << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "Window width:      " << m_windowWidth << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "Window height:     " << m_windowHeight << std::endl;
+	osg::notify(osg::DEBUG_INFO) << "Window spacing:    " << m_windowHorizontalSpacing << std::endl;
 	osg::notify(osg::DEBUG_INFO) << "Material sets:     " << m_materials.size() << std::endl;
 	osg::notify(osg::DEBUG_INFO) << std::endl;
 }
@@ -58,6 +78,7 @@ osg::ref_ptr<osg::Group> Building::createFromPolygon(osg::ref_ptr<Polygon> polyg
 	// Build walls
 	building->addChild(buildWalls(polygon, baseCoordinate, materialSet));
 	// Build windows
+	building->addChild(buildWindows(polygon, baseCoordinate, materialSet));
 	// Build roof
 	building->addChild(buildRoof(polygon, baseCoordinate, materialSet));
 	return building;
@@ -79,7 +100,8 @@ osg::ref_ptr<osg::Geode> Building::buildWalls(osg::ref_ptr<Polygon> polygon, osg
 {
 	osg::ref_ptr<osg::Geode> wallGeode = new osg::Geode;
 	osg::Vec4 wallColor = osg::Vec4(1.0, 1.0, 1.0, 1.0);
-	osg::ref_ptr<osg::Geometry> wallGeometry = extrudePolygon(polygon, baseCoordinate, m_roofHeight, wallColor);
+	double wallHeight = double(m_numberOfFloors) * m_floorHeight;
+	osg::ref_ptr<osg::Geometry> wallGeometry = extrudePolygon(polygon, baseCoordinate, wallHeight, wallColor);
 	// Apply material
 	osg::ref_ptr<osg::StateSet> stateSet = MaterialLibrary::instance().materialFromId(materialSet->wallMaterialId());
 	wallGeode->setStateSet(stateSet);
@@ -87,22 +109,89 @@ osg::ref_ptr<osg::Geode> Building::buildWalls(osg::ref_ptr<Polygon> polygon, osg
 	return wallGeode;
 }
 
+osg::ref_ptr<osg::Geode> Building::buildWindows(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, osg::ref_ptr<MaterialSet> materialSet)
+{
+	osg::ref_ptr<osg::Geode> windowGeode = new osg::Geode;
+	osg::ref_ptr<osg::Geometry> windowGeometry = new osg::Geometry;
+	
+	// Add vertices to window 
+	osg::ref_ptr<osg::Vec3Array> vertexArray = new osg::Vec3Array();
+	osg::ref_ptr<osg::Vec3Array> normalArray = new osg::Vec3Array();
+	osg::ref_ptr<osg::Vec2Array> textureArray = new osg::Vec2Array();
+
+	size_t numberOfPoints = polygon->points()->size();
+	for (size_t p = 0; p < (numberOfPoints - 1); ++p) {
+		osg::Vec2 point1 = polygon->points()->at(p) - baseCoordinate;
+		osg::Vec2 point2 = polygon->points()->at(p+1) - baseCoordinate;
+		double wallLength = (point2 - point1).length();
+		// Calculate how many windows fits on wall
+		double numberOfWindowsHorizontal = std::floor((wallLength - m_windowHorizontalSpacing) / (m_windowHorizontalSpacing + m_windowWidth));
+		if (numberOfWindowsHorizontal > 1) {
+			// Position windows on center of wall
+			double sideOffset = (wallLength - (numberOfWindowsHorizontal * m_windowWidth) 
+				- ((numberOfWindowsHorizontal-1) * m_windowHorizontalSpacing)) * 0.5;
+		
+			osg::Vec2 pNormal = point2 - point1;
+			pNormal.normalize();
+			// Create window geometry per floor
+			for (unsigned int floorId = 0; floorId < m_numberOfFloors; ++floorId) {
+				double windowHeight = polygon->height() + (double) floorId * m_floorHeight + m_windowVerticalSpacing;
+				double windowOffset = sideOffset;
+				// For each window
+				for (unsigned int windowId = 0; windowId < (unsigned int)numberOfWindowsHorizontal; ++windowId) {
+					osg::Vec2 wPoint1 = point1 + pNormal*windowOffset;
+					windowOffset += m_windowWidth;
+					osg::Vec2 wPoint2 = point1 + pNormal*windowOffset;
+					windowOffset += m_windowHorizontalSpacing;
+					buildWindowQuad(wPoint1, wPoint2, 
+						windowHeight, 
+						vertexArray, normalArray, textureArray);
+				}
+			}
+		}
+	}
+
+	windowGeometry->setVertexArray(vertexArray);
+	windowGeometry->addPrimitiveSet( new osg::DrawArrays(GL_QUADS, 0, vertexArray->size()) );
+	windowGeometry->setNormalArray(normalArray);
+	windowGeometry->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
+	osg::ref_ptr<osg::Vec4Array> colorArray = new osg::Vec4Array;
+	osg::Vec4 windowColor = osg::Vec4(1.0, 1.0, 1.0, 1.0);
+	colorArray->push_back(windowColor);
+	windowGeometry->setColorArray(colorArray);
+	windowGeometry->setColorBinding(osg::Geometry::BIND_OVERALL);
+	windowGeometry->setTexCoordArray(0, textureArray);
+	
+	// Apply material
+	osg::ref_ptr<osg::StateSet> stateSet = MaterialLibrary::instance().materialFromId(materialSet->windowMaterialId());
+	
+	// Windows should be drawn on top of buildings
+	stateSet->setAttributeAndModes(m_polygonOffset, osg::StateAttribute::OVERRIDE | osg::StateAttribute::ON);
+	
+	windowGeode->setStateSet(stateSet);
+	windowGeode->addDrawable(windowGeometry);
+	return windowGeode;
+}
+
+
+
 osg::ref_ptr<osg::Geode> Building::buildRoof(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, osg::ref_ptr<MaterialSet> materialSet)
 {
 	osg::ref_ptr<osg::Geode> roofGeode = new osg::Geode;
 	osg::Vec4 roofColor = osg::Vec4(1.0, 1.0, 1.0, 1.0);
 	osg::ref_ptr<osg::Geometry> roofGeometry = new osg::Geometry();
 	// Calculate height of building
-	double height = polygon->height() + m_roofHeight;
+	double wallHeight = double(m_numberOfFloors) * m_floorHeight;
+	double height = polygon->height() + wallHeight;
 	// Add vertices to roof
-	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array();
+	osg::ref_ptr<osg::Vec3Array> vertexArray = new osg::Vec3Array();
 	osg::ref_ptr<osg::Vec3Array> normalArray = new osg::Vec3Array();
 	osg::ref_ptr<osg::Vec2Array> textureArray = new osg::Vec2Array();
 	size_t numberOfPoints = polygon->points()->size();
 	osg::Vec2 centerPoint = polygon->center();
 	for (size_t p = 0; p < (numberOfPoints - 1); ++p) {
 		osg::Vec2 point = polygon->points()->at(p) - baseCoordinate;
-		vertices->push_back(osg::Vec3(point.x(), point.y(), height));
+		vertexArray->push_back(osg::Vec3(point.x(), point.y(), height));
 		normalArray->push_back(osg::Vec3(0.0, 0.0, 1.0));
 
 		osg::Vec2 texturePoint = polygon->points()->at(p) - centerPoint;
@@ -111,12 +200,12 @@ osg::ref_ptr<osg::Geode> Building::buildRoof(osg::ref_ptr<Polygon> polygon, osg:
 
 	// Add first point to close loop
 	osg::Vec2 point = polygon->points()->at(0) - baseCoordinate;
-	vertices->push_back(osg::Vec3(point.x(), point.y(), height));
+	vertexArray->push_back(osg::Vec3(point.x(), point.y(), height));
 	normalArray->push_back(osg::Vec3(0.0, 0.0, 1.0));
 	osg::Vec2 texturePoint = polygon->points()->at(0) - centerPoint;
 	textureArray->push_back(texturePoint);
 	// Pass the created vertex array to the points geometry object.
-	roofGeometry->setVertexArray(vertices);
+	roofGeometry->setVertexArray(vertexArray);
 	roofGeometry->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::POLYGON, 0, numberOfPoints));
 	roofGeometry->setNormalArray(normalArray);
 	roofGeometry->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
@@ -171,7 +260,6 @@ osg::ref_ptr<osg::Geometry> Building::extrudePolygon(osg::ref_ptr<Polygon> polyg
 	colorArray->push_back(color);
 	wallGeometry->setColorArray(colorArray);
 	wallGeometry->setColorBinding(osg::Geometry::BIND_OVERALL);
-
 	wallGeometry->setTexCoordArray(0, textureArray);
 	return wallGeometry;
 }
@@ -196,11 +284,50 @@ void Building::createVertexAndNormal(osg::Vec2 point1, osg::Vec2 point2,
 
 	osg::Vec2 textureCoord3 = osg::Vec2(startU, endV);
 	osg::Vec2 textureCoord4 = osg::Vec2(startU, 0);
-	// Vertexs
+	
+	// Vertices
 	vertexArray->push_back(vertex1);
 	vertexArray->push_back(vertex2);
 	vertexArray->push_back(vertex3);
 	vertexArray->push_back(vertex4);
+	
+	// Normals
+	osg::Vec3 normal = (vertex2 - vertex1)^(vertex4 - vertex1);
+	normal.normalize();
+	normalArray->push_back(normal);
+	normalArray->push_back(normal);
+	normalArray->push_back(normal);
+	normalArray->push_back(normal);
+
+	// Texture Coordinates
+	textureArray->push_back(textureCoord1);
+	textureArray->push_back(textureCoord2);
+	textureArray->push_back(textureCoord3);
+	textureArray->push_back(textureCoord4);
+}
+
+void Building::buildWindowQuad(osg::Vec2 point1, osg::Vec2 point2, 
+	double startHeight,
+	osg::ref_ptr<osg::Vec3Array> vertexArray, 
+	osg::ref_ptr<osg::Vec3Array> normalArray, 
+	osg::ref_ptr<osg::Vec2Array> textureArray) 
+{
+	osg::Vec3 vertex1 = osg::Vec3(point1.x(), point1.y(), startHeight);
+	osg::Vec3 vertex2 = osg::Vec3(point1.x(), point1.y(), startHeight + m_windowHeight);
+	osg::Vec3 vertex3 = osg::Vec3(point2.x(), point2.y(), startHeight + m_windowHeight);
+	osg::Vec3 vertex4 = osg::Vec3(point2.x(), point2.y(), startHeight);
+
+	osg::Vec2 textureCoord1 = osg::Vec2(0, 0);
+	osg::Vec2 textureCoord2 = osg::Vec2(0, 1);
+	osg::Vec2 textureCoord3 = osg::Vec2(1, 1);
+	osg::Vec2 textureCoord4 = osg::Vec2(1, 0);
+	
+	// Vertices
+	vertexArray->push_back(vertex1);
+	vertexArray->push_back(vertex2);
+	vertexArray->push_back(vertex3);
+	vertexArray->push_back(vertex4);
+
 	// Normals
 	osg::Vec3 normal = (vertex2 - vertex1)^(vertex4 - vertex1);
 	normal.normalize();
--- src/building.h
+++ src/building.h
@@ -12,6 +12,8 @@
 #include <osg/Referenced>
 #include <osg/Group>
 #include <osg/Geode>
+#include <osg/PolygonOffset>
+
 
 #include "polygon.h"
 #include "pugixml.hpp"
@@ -38,14 +40,15 @@ class MaterialSet : public osg::Referenced {
 
 class Building : public osg::Referenced {
 	public:
-		Building() : m_roofHeight(0.0), m_basementHeight(0.0), m_windowOffset(0.0) {};
+		Building();
 		void load(pugi::xml_node buildingNode);
 		osg::ref_ptr<osg::Group> createFromPolygon(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate);
-		void setParameters(double roof, double basement, double window) { m_roofHeight = roof; m_basementHeight = basement; m_windowOffset = window; }
+		std::string type() const { return m_type; }
 	protected:
 		~Building() {};
 		osg::ref_ptr<osg::Geode> buildBasement(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, osg::ref_ptr<MaterialSet> materialSet);
 		osg::ref_ptr<osg::Geode> buildWalls(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, osg::ref_ptr<MaterialSet> materialSet);
+		osg::ref_ptr<osg::Geode> buildWindows(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, osg::ref_ptr<MaterialSet> materialSet);
 		osg::ref_ptr<osg::Geode> buildRoof(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, osg::ref_ptr<MaterialSet> materialSet);
 		osg::ref_ptr<osg::Geometry> extrudePolygon(osg::ref_ptr<Polygon> polygon, osg::Vec2 baseCoordinate, double height, osg::Vec4 color);
 		void createVertexAndNormal(osg::Vec2 point1, osg::Vec2 point2, 
@@ -54,12 +57,24 @@ class Building : public osg::Referenced {
 			osg::ref_ptr<osg::Vec3Array> normalArray, 
 			osg::ref_ptr<osg::Vec2Array> textureArray,
 			double& startU);
+		void buildWindowQuad(osg::Vec2 point1, osg::Vec2 point2,
+			double startHeight,
+			osg::ref_ptr<osg::Vec3Array> vertexArray, 
+			osg::ref_ptr<osg::Vec3Array> normalArray, 
+			osg::ref_ptr<osg::Vec2Array> textureArray);
 		osg::ref_ptr<MaterialSet> getRandomMaterialSet(osg::Vec2 point);
 
-		double m_roofHeight;
 		double m_basementHeight;
-		double m_windowOffset;
+		double m_floorHeight;
+		unsigned int m_numberOfFloors;
+		double m_windowHorizontalSpacing;
+		double m_windowVerticalSpacing;
+		double m_windowHeight;
+		double m_windowWidth;
+		std::string m_type;
 		std::vector< osg::ref_ptr<MaterialSet> > m_materials;
+
+		static osg::ref_ptr<osg::PolygonOffset> m_polygonOffset;
 };
 
 typedef std::map< double, osg::ref_ptr<Building> > BuildingMap;
